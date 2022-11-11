882db8a60ca2000a2ee4016254b145b1be4e73b6
yoex <ytentertainmnet94@gmail.com>
2022-11-11 14:02:17 -0400

    checkpoint helper hardhat 2

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -16,6 +16,7 @@ const { network } = require("hardhat");
 //Y la exporto para que la pueda ver el hardhat deploy
 //module.exports.default = deployFunc;
 
+//Export el networkConfig desde el archvio helper
 const { networkConfig } = require("../helper-hardhat-config");
 
 module.exports = async ({ getNameAccounsts, deployments }) => {
@@ -23,4 +24,8 @@ module.exports = async ({ getNameAccounsts, deployments }) => {
   const { deployer } = await getNameAccounsts(); //Para obetner el nombre de las cuentas con private key que creamos para la network
 
   const chainId = network.config.chainId;
+
+  //puedo obtener el eth price address usando el chain id
+
+  const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
 };
